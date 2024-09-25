2b0a0128bfba6aa0d711e2c42959ff31dc93bc45
Agnieszka <agni.banaszek@gmail.com>
2024-09-25 18:07:12 +0200

    Create functions in ReviewController

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -3,27 +3,64 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Review;
+use App\Models\Hotel;
+use Illuminate\Support\Facades\Auth;
 
 class ReviewController extends Controller
 {
-    public function showHotelReviews() 
+    public function showUserReview()
     {
-
+        $user = Auth::user();
+        $reviews = Review::where('UserID', $user->id)->get();
+        $responseData = [];
+        foreach ($reviews as $reviewItem) {
+            $responseData[] = [
+                'id' => $reviewItem->id,
+                'title' => $reviewItem->title,
+                'rate' => $reviewItem->rate,
+                'description' => $reviewItem->description,
+                'hotel' => $reviewItem->hotel->name
+            ];
+        }
+        return response()->json($responseData);
     }
-    public function showUserReviews() 
+    public function showHotelReview($id)
     {
-
+        $hotel = Hotel::find($id);
+        $reviews = Review::where('HotelID', $hotel->id)
+            ->with('user')
+            ->get();
+        return response()->json($reviews);
     }
-    public function createReview() 
+    public function createReview(Request $request)
     {
-
+        $validatedData = $request->validate([
+            'title' => 'required|string',
+            'rate' => 'required',
+            'description' => 'required|string',
+            'hotelId' => 'required',
+            'userId' => 'required'
+        ]);
+        $review = Review::create([
+            'title' => $validatedData['title'],
+            'rate' => $validatedData['rate'],
+            'description' => $validatedData['description'],
+            'HotelID' => $validatedData['hotelId'],
+            'UserID' => $validatedData['userId']
+        ]);
+        return response()->json(['message' => 'Data added successfully', 'data' => $review], 201);
     }
-    public function DeleteReview() 
+    public function DeleteReview($id)
     {
+        $review = Review::find($id);
 
-    }
-    public function UpdateReview() 
-    {
+        if (!$review) {
+            return response()->json(['message' => 'Review not found'], 404);
+        }
 
+        $review->delete();
+
+        return response()->json(['message' => 'Review deleted successfully']);
     }
 }
